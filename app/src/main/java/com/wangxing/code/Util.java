c87540467e17f7958f99fcac68207b9735ea4250
wangxxx <764690578@qq.com>
2018-06-11 17:09:42 +0800

    V2

---
+++ app/src/main/java/com/wangxing/code/Util.java
@@ -0,0 +1,16 @@
+package com.wangxing.code;
+
+import android.content.Context;
+
+import com.wangxing.code.utils.ToastUtil;
+
+/**
+ * Created by WangXing on 2018/6/11.
+ */
+public class Util {
+
+    public static void show (Context context){
+        ToastUtil.showShort(context,"util");
+    }
+
+}
